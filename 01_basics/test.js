38f2073c73a287ebecfa311034902d11ce4fdafe
Chaudhry Wasam Ur Rehman <140172148+ChWasam@users.noreply.github.com>
2023-11-08 22:04:25 +0000

    setup Concepts

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,17 @@
 console.log("Wasam");
 //  write in console => node 01_basics/test.js 
+
+
+
+//  Difference between test.js and test.txt file is 
+// kah konsi file ham kisi software me inject karwa kah execute karwa sakta han 
+// software == interpreter or compiler == in ka pass hoti ha syntax structure smajhna ke capability
+
+// javascript => buhat salo tak software chupa hua tha browser me 
+// now software also standalone javascript=> thatswhy backend me 
+
+// Backend deno or node.js 
+
+
+// online code execution environment 
+// Github
