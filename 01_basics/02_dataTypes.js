7e2c97992d651d7264e35152a73c4196a95def31
Chaudhry Wasam Ur Rehman <140172148+ChWasam@users.noreply.github.com>
2023-11-09 14:32:25 +0500

    DataTypes and ECMA standards

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version (classes, modules, arrow function etc)
+
+
+// code readability should be high 
+
+// documentation => mdn(by mozila) + tc39.es
+// ecma script standard likhti ha 
+
+// premitive datatypes => string , number, boolean, null, undefined, symbol 
+// number => range jo kah sirf memory adress me milta ha => aus sa bara number ho to bigint use karo 
+// null => actually a standalone value 
+
+// undefined => value declare ke ha lakin assign nahi ke 
+// while null is the representation of empty value 
+
+// e.g server sa request bhaji aur aus sa kaha kah mujha temperature btao  
+// aur server me koi dikkat a gae ha 
+// agar temp zero bhaj data to zero to temp ha 
+// here will prefer null 
+// matlab abhi temp aya hi nahi ha 
+
+// symbol => used in react to find uniqueness
+
+
+// object 
+
+// typeof 
+
+// type of null => object 
+// type of undefined => undefined 
+
