d54e537373fcb4014b7e0fdbd1430f5ca33d8765
Chaudhry Wasam Ur Rehman <140172148+ChWasam@users.noreply.github.com>
2023-11-09 16:30:41 +0500

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,10 +1,10 @@
 let score =NaN
 
-console.log(typeof score)  // number
+// console.log(typeof score)  // number
 
 
 let valueInNumber=Number(score) // ab value number me convert ho gae ha 
-console.log(valueInNumber); // here NaN if score ="33abc"
+// console.log(valueInNumber); // here NaN if score ="33abc"
   // here 0 if score =null 
  // here NaN if score =undefined 
  // here 1 if score =true 
@@ -15,7 +15,7 @@ console.log(valueInNumber); // here NaN if score ="33abc"
  let isLogedIn = 1 // true 
 
  let booleanIsLoggedIn = Boolean(isLogedIn)
- console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
 
  // 1=> true ; 0=> false 
  // "" => false 
@@ -25,4 +25,33 @@ console.log(valueInNumber); // here NaN if score ="33abc"
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *****************************operations********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// code should not look like messy 
+// write in pa+rentrhesis 
+// console.log( 3+((4*5)/2) );
+
+// console.log(+true); // 1
+// console.log(+"");//0
+// console.log(+false); // 0
+// lakin asa tricky behaviour likhna nahi chahia 
+// coding ka andar readability sab sa zyada imp ha 
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+// prefix and postfix operators => read from mdn
+// typeconversion from tc39 website 
+
+
+
+
+
