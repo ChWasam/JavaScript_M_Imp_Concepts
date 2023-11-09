2ccb519bd21c0192c368e8114e7c0d0a81f39679
Chaudhry Wasam Ur Rehman <140172148+ChWasam@users.noreply.github.com>
2023-11-09 16:50:29 +0500

    Comparison of DataTypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+console.log(null > 0); // false 
+console.log(null == 0);// false 
+console.log(null >= 0);// true
+
+// The reason is that an equality check == and comparisons > ,<, >= , <= work differently in JS.
+
+// Comparisons convert null to a number, treating it as 0.
+// That's why (3) null >= 0 is true and (1) null >0 is false 
+
+
+console.log(undefined == 0);// false 
+console.log(undefined > 0);// false 
+console.log(undefined < 0);//false
+
+// aupar wala sab code ko avoid hi karna chahia 
+
+// strict check === 
+// cleck value +datatype
+
+// Note: we always do clean code isi ka sab sa zyada value ha 
+
+
